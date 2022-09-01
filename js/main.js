7895a5ae992afe9ff2bd6609952bdc541b51e47d
yuseon4455 <105402328+yuseon4455@users.noreply.github.com>
2022-09-01 14:04:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 // main-header nav
 
 $(function () {
-    $("ul.gnb>li").hover(function () {
-      $("ul.sub, .nav-bg").stop().slideDown();
-    }, function () {
-      $("ul.sub, .nav-bg").stop().slideUp();
-    });
+  $("ul.gnb>li").hover(function () {
+    $("ul.sub, .nav-bg").stop().slideDown();
+  }, function () {
+    $("ul.sub, .nav-bg").stop().slideUp();
   });
+});
+
+
+
+
+// main-visual slide
+
+$(function () {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    speed: 1000,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },
+
+  });
+});
